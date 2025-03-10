8638e2c25dba00bcf0ef9e6b92ac03e842227a57
Md Emamudin <mdemamudin726@gmail.com>
2025-03-10 19:35:13 +0530

    added vercel file

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const app = express();
 const bodyparser = require('body-parser');
 app.use(cors({
-    // origin:'*',     // frontend url
+    // origin:'https://axis-exports.vercel.app',     // frontend url
     origin:'http://localhost:5173',
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
