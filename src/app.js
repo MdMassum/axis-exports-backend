8730c98e3255fee96343ce8dbd473105b47eb1b8
Md Emamudin <mdemamudin726@gmail.com>
2025-06-27 20:03:21 +0530

    bug fix

--- src/app.js
+++ src/app.js
@@ -13,19 +13,32 @@ const contact = require('../src/routes/contactRoute')
 
 const app = express();
 const bodyparser = require('body-parser');
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://axis-exports.vercel.app',
+  'https://www.axisinternationalexports.com'
+];
+
 app.use(cors({
-    // origin:'https://axis-exports.vercel.app',     // frontend url
-    origin:'http://localhost:5173' || 'https://axis-exports.vercel.app' || 'https://www.axisinternationalexports.com',
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: [
-        "Content-Type",
-        "Authorization",
-        "Cache-Control",
-        "Expires",
-        "Pragma",
-      ],
-    credentials: true
-}))
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Cache-Control",
+    "Expires",
+    "Pragma",
+  ],
+  credentials: true
+}));
 
 // to access req.body
 app.use(bodyparser.json()); 
@@ -33,6 +46,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 //available routes
+app.get('/',(req,res)=>{
+  res.json({
+    success:true,
+    message:"Server Up and Running"
+  })
+})
 app.use('/api/v1',products);
 app.use('/api/v1',users);
 app.use('/api/v1',order);
